ae41e365571d1805d4a36774b3eaec2e691eb05b
haceryaman <yaman.hacer@outlook.com>
2024-06-04 13:27:39 +0300

    font adding fixed

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,33 @@
 import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+// splash screen olmasını sağlayacak
+//SplashScreen.preventAutoHideAsync(); // takılı kalmasına sebep oluyo
 
 const Layout=()=>{
-    return <Stack></Stack>
+    
+    const [fontsLoaded]= useFonts({
+        DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+        DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+        DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+
+    })
+    
+    const onLayoutRootView= useCallback(
+        async ()=> {
+            if(fontsLoaded){
+                await SplashScreen.hideAsync();
+            }
+        }, [fontsLoaded])
+
+
+    if (!fontsLoaded) return null
+
+    //fontun yükleneceğinden emin olduk
+        
+    return <Stack onLayout={onLayoutRootView}></Stack>
 }
 
 export default Layout
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput,TouchableOpacity,Image, FlatList } from 'react-native'
+import { router, useRouter } from 'expo-router'
+import { icons, SIZES } from '../../../constants'
 
 import styles from './welcome.style'
 
 const Welcome = () => {
+  const router= useRouter();
   return (
     <View>
-      <Text>Welcome</Text>
+
+      <View style={styles.container}>
+        <Text style={styles.userName}> Hello Hacer! </Text>
+        <Text style={styles.welcomeMessage}> Find your perfect job! </Text>
+      </View>
+
+      <View style={styles.searcContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={styles.searchInput}>
+            
+          </TextInput>
+
+        </View>
+
+      </View>
+
     </View>
   )
 }
