b85a189ea944b63da34514c45be9803820ad501d
haceryaman <yaman.hacer@outlook.com>
2024-06-04 21:02:17 +0300

    select job type button added

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, TextInput,TouchableOpacity,Image, FlatList } from 'react-native'
 import { router, useRouter } from 'expo-router'
 import { icons, SIZES } from '../../../constants'
 
 import styles from './welcome.style'
 
+const jobTypes= ["Full-time", "Part-time", "Contractor"];
+
 const Welcome = () => {
+
   const router= useRouter();
+  const [activeJobType, setActiveJobType] = useState("Full-time")
+
+
   return (
     <View>
 
@@ -15,16 +21,38 @@ const Welcome = () => {
         <Text style={styles.welcomeMessage}> Find your perfect job! </Text>
       </View>
 
-      <View style={styles.searcContainer}>
+      <View style={styles.searchContainer}>    
         <View style={styles.searchWrapper}>
-          <TextInput style={styles.searchInput}>
-            
-          </TextInput>
+          <TextInput                  // SEARCH BAR 
+          style={styles.searchInput} 
+          value='' 
+          onChange={()=>{}} 
+          placeholder='What are you looking for?'
+          /></View>
 
-        </View>
-
+        <TouchableOpacity style={styles.searchBtn} onPress={()=>{}}> 
+          <Image source={icons.search}  // SEARCH BUTTON
+          resizeMode='contain'
+          style={styles.searchBtnImage}/>
+        </TouchableOpacity>
       </View>
 
+      <FlatList
+        data={jobTypes}
+        horizontal
+        renderItem={({ item }) => (
+          <TouchableOpacity style={styles.tab(activeJobType, item)} onPress={()=>{
+            setActiveJobType(item)
+            router.push(`/search/${item}`)
+          }}>
+            <Text style={styles.tabText(activeJobType, item)}>
+              {item}
+            </Text>
+          </TouchableOpacity>
+        )}
+        keyExtractor={(item) => item}
+        contentContainerStyle={{columnGap:SIZES.small}}
+      />
     </View>
   )
 }
